9a61769d0962e9615a2d51b65036951566013b6d
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2022-06-25 06:34:05 +0200

    Correction fichier "head".

--- affichage/head.php
+++ affichage/head.php
@@ -1,9 +1,19 @@
 <?php
 session_start();
-
 $title = 'S-B-M';
 $title2 = 'Simple Blog Machine';
 $css = 'css/master.css';
+// Création du meta description dynamique
+$selectD = "SELECT `titre`
+FROM `articles` LIMIT 20";
+$paramDescription = array();
+$dataDescription = new RCUD($selectD, $paramDescription);
+$dataD = $dataDescription->READ();
+$description = NULL;
+foreach ($dataD as $key => $value) {
+  $description = $value['titre'].', '.$description;
+}
+
  ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
